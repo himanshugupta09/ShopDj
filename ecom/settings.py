e9791bf9bd093d14118de6c619beb75f24070f7e
Himanshu Gupta <hg7984152@gmail.com>
2026-03-15 19:41:43 +0530

    Inital changes more

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ecom.urls'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')      # your Gmail
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')   # 16-char app password
+DEFAULT_FROM_EMAIL = 'ShopDjango <himanshu033.psit@gmail.com>'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
